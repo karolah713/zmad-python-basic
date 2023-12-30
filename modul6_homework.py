111a5b6a9b4b076011d20a00bc9ca2d49979c989
karolah713 <karolah713@gmail.com>
2023-12-30 20:48:27 +0100

    Add exc.3&4

--- modul6_homework.py
+++ modul6_homework.py
@@ -60,6 +60,9 @@ def cleanUp_orders2():
 ###############################################################################################################################
 
 #ZADANIE 2
+'''Napisz funkcję, która przyjmuje listę plików oraz nazwę nowego pliku jako argumenty wejściowe. 
+Funkcja scala zawartość plików w jeden plik wynikowy o podanej wcześniej nazwie.'''
+
 def create_bulk_file(bulkFileName, *args):
     col = ['Kraj', 'Sprzedawca', 'Data zamowienia', 'idZamowienia', 'Utarg']
     with open(bulkFileName, 'w', newline='') as target:
@@ -84,6 +87,46 @@ def create_bulk_file2(bulkFileName, *args):
     concatenated_df = pd.concat(concatenated_list, ignore_index=True)
     concatenated_df.to_csv(bulkFileName, index=False)
 
-create_bulk_file2('zamowienia_all2.csv','zamowienia_polska2.csv','zamowienia_niemcy2.csv')
+#create_bulk_file2('zamowienia_all2.csv','zamowienia_polska2.csv','zamowienia_niemcy2.csv')
+
+###############################################################################################################################
+
+#ZADANIA POWTÓRZENIOWE
+
+#ZADANIE 3
+'''Napisz funkcję, która będzie zwracała 3 największe wartości z listy numerycznej. 
+Lista jest parametrem wejściowym funkcji.'''
+
+def return_3max(numeric_list):
+    max_3_num = []
+    for _ in range(3):
+        max_number = max(numeric_list)
+        max_3_num.append(max_number)
+        numeric_list.remove(max_number)
+    return max_3_num
+
+#print(return_3max([1,2,3,6,8,100,87]))
+
+###############################################################################################################################
+
+#ZADANIE 4
+'''Mając listę mieszana = [1, 2.3, ‘Zbyszek’, 5, ‘Marian’, 3.0] napisz funkcję, 
+która zapisze wartości dla każdego typu do słownika gdzie pod kluczem równym nazwie typu 
+zmiennej (int, float, str, itp.) wartością będzie lista elementów z listy 'mieszana' danego typu. 
+Przykład: {'int': [1,5], 'str': ['Zbyszek','Marian']} itd.'''
+
+mieszana = [1, 2.3, "Zbyszek", 5, "Marian", 3.0]
+def create_type_dict(list):
+    type_dict = {'int':[], 'str':[], 'float':[]}
+    for elem in mieszana:
+        if type(elem) == int:
+            type_dict['int'].append(elem)
+        if type(elem) == str:
+            type_dict['str'].append(elem)
+        if type(elem) == float:
+            type_dict['float'].append(elem)
+    return type_dict
+
+#print(create_type_dict(mieszana))
 
 ###############################################################################################################################
